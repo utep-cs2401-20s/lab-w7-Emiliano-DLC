8f7173d31015d7a0f7249e43a3ac8bcac7cf13b3
Emiliano-DLC <emiliano_r_d_l_c@outlook.com>
2020-03-03 21:07:57 -0700

    working on sorting algorithm

--- java/src/sortOfSort.java
+++ java/src/sortOfSort.java
@@ -1,12 +1,25 @@
 public class sortOfSort {
     public void sortOfSort(int[] arr) {
-        int greater = -1000;
+        int firstValue;
+        int nextItem;
         int temp;
 
         for (int i = 0; i < arr.length; i++) {
-            if (greater < arr[i]) {
-                greater = arr[i];
+            firstValue = arr[i];
+            nextItem = arr[i+1];
+            if(firstValue<nextItem){
+                temp = firstValue;
+                firstValue = nextItem;
+                nextItem = temp;
             }
+        }
+
+    }
+    public void main(String[] args){
+        int[] arr = {2,3,4,8,1,7,9,3,1};
+        sortOfSort(arr);
+        for(int i = 0; i<arr.length; i++){
+            System.out.println(arr);
         }
     }
 }
